c552bcadd4685c62fc52998c57c42f06a9e0c45f
Davide Mecugni <davidemecugni@gmail.com>
2025-01-19 08:36:53 +0100

    Uploader

--- src/uploader.py
+++ src/uploader.py
@@ -2,17 +2,18 @@ import csv
 
 with open("test.csv", "r") as f:
     data = csv.reader(f)
-    results = [row[0] for row in data]
+    results = [row for row in data]
 
 print(len(results))
 
 i=1
-with open("result_formatted.csv", "w") as f:
+with open("risultati_f.csv", "w") as f:
     csv_writer = csv.writer(f)
 
     csv_writer.writerow(["row_id","result"])
 
     for r in results:
+        r = ",".join(r)
         r = '"' + r + '"'
         f.write(f"{i},{r}\n")
         i+=1
